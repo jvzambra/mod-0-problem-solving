3a32698c1ef6929a65e377659d0ac29b3b9f20d0
Jorge Zambrana <zee7man@yahoo.com>
2023-07-18 18:49:39 -0600

    Add additional corrections

--- challenge1.rb
+++ challenge1.rb
@@ -1,11 +1,12 @@
 # This program will print only strings of exactly 4 characters from an array .
 # Define an array of strings.
 weekdays = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri'] 
-# Incorporate the each method by adding .each to the variable name, follow it by do and identify a name for a singular element from the array.
+# Incorporate the each method by adding .each to the variable name, follow it by do and identify a name for a singular element from the array to include within pipes.
 weekdays.each do |weekday|
 # Add the puts command so print output when the code is run, and include a conditional if statement so that the individual weekday element is 
 # printed only if the character length is equal to 4, using the length method and comparison operater "==". 
 puts weekday if weekday.length == 4
 # Add "end" to complete the code block and allow the each method to be called.
 end
-#=> The the code is run, Tues & Thur are the two elements that are printed.
+# When the code is run, only the elements equal to 4 characters are printed:
+#=> Tues Thur
